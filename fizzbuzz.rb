a04419237e405254888da607761302672dd1e443
Elizabeth Karst <lkarst@outlook.com>
2018-08-11 01:15:50 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(x)
-  if x % 3 == 0 && x % 5 == 0
+  if x % 5 == 0
     return "FizzBuzz"
-  elseif x % 5 == 0
+  elseif x % 3 == 0 && x % 5 == 0
     return "Buzz"
   end
 end
